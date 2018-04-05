c1edacfc34f6c65edff0b435d322303c1a914357
casgaromio <casgaromio@naver.com>
2018-04-06 00:06:27 +0900

    laravel ecommerce 1

---
+++ app/Product.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Nicolaslopezj\Searchable\SearchableTrait;
+
+class Product extends Model
+{
+    use SearchableTrait;
+    /**
+     * Searchable rules.
+     *
+     * @var array
+     */
+    protected $searchable = [
+        /**
+         * Columns and their priority in search results.
+         * Columns with higher values are more important.
+         * Columns with equal values have equal importance.
+         *
+         * @var array
+         */
+        'columns' => [
+            'products.name' => 10,
+            'products.details' => 5,
+            'products.description' => 2,
+        ],
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category');
+    }
+
+    public function presentPrice()
+    {
+        return number_format($this->price / 100, 2);
+    }
+
+    public function scopeMightAlsoLike($query)
+    {
+        return $query->inRandomOrder()->take(4);
+    }
+}
